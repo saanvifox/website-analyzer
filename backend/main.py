7429dd5081935ff1b9f8f79602674188fbc62828
Saanvi2222 <saanvi200620@gmail.com>
2026-07-16 15:10:03 +0530

    Connect deployed frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173"
+        "https://website-analyzer-pi.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
